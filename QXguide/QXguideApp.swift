b8c6ef630af81d8b18089dc273182eb0220dfacb
Ardak <ardaktursunbayev4@gmail.com>
2024-03-10 17:03:42 +0600

    Initial Commit

---
+++ QXguide/QXguideApp.swift
@@ -0,0 +1,17 @@
+//
+//  QXguideApp.swift
+//  QXguide
+//
+//  Created by Ardak Tursunbayev on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct QXguideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
